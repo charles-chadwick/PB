148afb57522ccc7609347d5d88950645fea35a42
Charles Chadwick <charles@mutuallyassured.com>
2025-05-05 14:05:08 -0700

    Seeding the users and profiles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,9 @@ class UserSeeder extends Seeder {
 		DB::table('users')
 		  ->truncate();
 
+		DB::table('profiles')
+		  ->truncate();
+
 		$admin = User::create([
 			"role"          => UserRole::SuperAdmin,
 			"first_name"    => "John",
@@ -34,21 +37,18 @@ class UserSeeder extends Seeder {
 				strtolower("{$character['first_name']}.{$character['last_name']}@example.com"));
 
 			$role = match ( true ) {
-				$counter <= 1  => UserRole::Doctor,
-				$counter <= 4  => UserRole::Nurse,
+				$counter <= 1 => UserRole::Doctor,
+				$counter <= 4 => UserRole::Nurse,
 				$counter <= 8 => UserRole::Staff,
-				default        => UserRole::Patient,
+				default       => UserRole::Patient,
 			};
 
 			// because we truncate the table every time, the user IDs will be 2-10 for any staff.
 			$created_by_id = 1;
 			$created_at = fake()->dateTimeBetween("2020-01-01 08:00:00", "2021-01-01 08:00:00");
-			if ($role === UserRole::Patient) {
+			if ( $role === UserRole::Patient ) {
 				$created_by_id = fake()->randomElement(range(2, 10));
-				$created_at = fake()->dateTimeBetween(
-					User::find($created_by_id)->created_at,
-					"-1 week",
-				);
+				$created_at = fake()->dateTimeBetween(User::find($created_by_id)->created_at, "-1 week");
 			}
 
 			$user = User::create([
@@ -58,9 +58,24 @@ class UserSeeder extends Seeder {
 				"email"         => $email,
 				"password"      => bcrypt('password'),
 				"created_by_id" => $created_by_id,
+				"updated_by_id" => $created_by_id,
 				"created_at"    => $created_at,
 				"updated_at"    => $created_at,
 			]);
+
+			if ( $role === UserRole::Patient ) {
+				$user->profile()
+					 ->create([
+						 'gender'        => $character[ "gender" ],
+						 'dob'           => fake()
+							 ->dateTimeBetween("-80 years", "-1 years")
+							 ->format('Y-m-d'),
+						 "created_by_id" => $created_by_id,
+						 "updated_by_id" => $created_by_id,
+						 "created_at"    => $created_at,
+						 "updated_at"    => $created_at,
+					 ]);
+			}
 
 			$counter++;
 		}
