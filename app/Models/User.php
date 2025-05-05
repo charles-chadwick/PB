fd155243d76a53e361be7a057e7aeaa933708417
Charles Chadwick <charles@mutuallyassured.com>
2025-05-05 11:27:59 -0700

    Changed User model to conform to Laravel standards, but with our base class too

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,65 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 
-class User extends Authenticatable
-{
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+class User extends Base implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract {
+	use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'role',
+		'first_name',
+		'middle_name',
+		'last_name',
+		'email',
+		'password',
+		'email_verified_at',
+	];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+	/**
+	 * Get the attributes that should be cast.
+	 *
+	 * @return array<string, string>
+	 */
+	protected function casts() : array {
+		return [
+			'id'                => 'integer',
+			'email_verified_at' => 'timestamp',
+			'password'          => 'string',
+		];
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @param  array  $attributes
+	 */
+	public function __construct( array $attributes = [] ) {
+		parent::__construct($attributes);
+	}
+
+	public function profile() {
+		return $this->hasOne(Profile::class, "patient_id", "id");
+	}
 }
